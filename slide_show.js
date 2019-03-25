3739825c4a4e17692b29b186f274401b481afd04
lauryngaffney <36428835+lauryngaffney@users.noreply.github.com>
2019-03-24 19:18:35 -0500

    Add files via upload

---
+++ slide_show.js
@@ -0,0 +1,15 @@
+var slideShow;
+
+window.onload = function () {
+    var params = {
+        listId     : "image_list",
+        imageId    : "image",
+        previousId : "btn_previous",
+        playId     : "btn_play",
+        playPauseId: "img_play_pause",
+        nextId     : "btn_next",
+        speedId    : "btn_speed"
+    }
+
+    slideShow = new SlideShow( params );
+}
